1d322b62d0c74155ef83383cbfc75326f27bcb03
Lino Virgen <lino.virgen96@gmail.com>
2018-11-10 17:34:27 -0600

    code translator done

---
+++ Translation/files/file2.py
@@ -0,0 +1,2 @@
+def foo():
+    print("Artificial Intelligence Initiative")
--- Translation/translatetest.py
+++ Translation/translatetest.py
@@ -1,12 +1,19 @@
-import sys
+import codecs
+import os
 from transliterate import translit, get_available_language_codes
 
 def test():
-    text = "Artificial Intelligence Innitiative"
-    
-    cy = translit(text,'ru')
-    print(cy.encode('utf8'))
+    for name in os.listdir('./files'):
+        f = open('./files/' + name, 'r')
+        cy_name = translit(name,'ru')
 
+        with codecs.open('./cy_files/' + cy_name, "w", "utf-16") as cy_file:
+            for line in f:
+                cy_line = translit(line,'ru')
+                cy_file.write(cy_line)
+
+        cy_file.close()
+        f.close()
 
 if __name__ == "__main__":
     test()
