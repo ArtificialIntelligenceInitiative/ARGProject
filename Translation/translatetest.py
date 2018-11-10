ecc830a6f0bd69758781c75eace138504b832939
Lino Virgen <lino.virgen96@gmail.com>
2018-11-10 16:03:03 -0600

    translation

--- Translation/translatetest.py
+++ Translation/translatetest.py
@@ -1,10 +1,11 @@
+import sys
 from transliterate import translit, get_available_language_codes
 
 def test():
     text = "Artificial Intelligence Innitiative"
     
     cy = translit(text,'ru')
-    print(cy.decode('utf8'))
+    print(cy.encode('utf8'))
 
 
 if __name__ == "__main__":
